1b345d91e667d868f76ac989f6906270f0cf94be
Kevin Ludwig <signing@valaphee.com>
2022-01-18 04:41:17 +0100

    Add exploit test

--- src/test/kotlin/com/valaphee/jackson/dataformat/nbt/NbtFactoryTest.kt
+++ src/test/kotlin/com/valaphee/jackson/dataformat/nbt/NbtFactoryTest.kt
@@ -1,18 +1,18 @@
- /*
- * Copyright (c) 2022, Valaphee.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+* Copyright (c) 2022, Valaphee.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package com.valaphee.jackson.dataformat.nbt
 
@@ -21,6 +21,10 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.ByteArrayOutputStream
+import java.io.EOFException
+import kotlin.system.measureTimeMillis
 
 /**
  * @author Kevin Ludwig
@@ -102,5 +106,27 @@ class NbtFactoryTest {
     fun bigTest() {
         val objectMapper = ObjectMapper(NbtFactory()).apply { registerKotlinModule() }
         objectMapper.readValue<Any>(javaClass.getResource("/bigtest.nbt")!!)
+    }
+
+    @Test
+    fun exploit() {
+        val objectMapper = ObjectMapper(NbtFactory()).apply { registerKotlinModule() }
+
+        // 1. Run
+        ByteArrayOutputStream().use {
+            it.writeBytes(byteArrayOf(NbtType.List.ordinal.toByte(), 0x00, 0x00))
+            repeat(100) { _ -> it.writeBytes(byteArrayOf(NbtType.List.ordinal.toByte(), 0xFF.toByte(), 0xFF.toByte(), 0xFF.toByte(), 0xFF.toByte())) }
+            assertThrows<EOFException> { objectMapper.readValue<Any>(it.toByteArray()) }
+        }
+
+        println("${Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()}B used")
+        println("Took " + measureTimeMillis {
+            ByteArrayOutputStream().use {
+                it.writeBytes(byteArrayOf(NbtType.List.ordinal.toByte(), 0x00, 0x00))
+                repeat(100) { _ -> it.writeBytes(byteArrayOf(NbtType.List.ordinal.toByte(), 0xFF.toByte(), 0xFF.toByte(), 0xFF.toByte(), 0xFF.toByte())) }
+                assertThrows<EOFException> { objectMapper.readValue<Any>(it.toByteArray()) }
+            }
+        } + "ms")
+        println("${Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()}B used")
     }
 }
